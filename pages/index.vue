c4d2c5831dc2d350b80ef9d06b0118d611a4d5f8
serkan_korkac <korkacserkan@gmail.com>
2021-11-09 13:26:00 +0300

    index blogs

--- pages/index.vue
+++ pages/index.vue
@@ -5,25 +5,33 @@
     <h1 class="font-bold text-4xl">Blog Posts</h1>
     <ul class="flex flex-wrap">
       <li
-        v-for="article of articles"
-        :key="article.slug"
+        v-for="article of blogs"
+        :key="article.ID"
         class="xs:w-full md:w-1/2 px-2 xs:mb-6 md:mb-12 article-card"
       >
         <NuxtLink
-          :to="{ name: 'blog-slug', params: { slug: article.slug } }"
-          class="flex transition-shadow duration-150 ease-in-out shadow-sm hover:shadow-md xxlmax:flex-col"
+          :to="{ name: 'blog-slug', params: { slug: article.ID } }"
+          class="
+            flex
+            transition-shadow
+            duration-150
+            ease-in-out
+            shadow-sm
+            hover:shadow-md
+            xxlmax:flex-col
+          "
         >
           <img
             v-if="article.img"
             class="h-48 xxlmin:w-1/2 xxlmax:w-full object-cover"
-            :src="article.img"
+            :src="importImage(article.img)"
           />
 
           <div
             class="p-6 flex flex-col justify-between xxlmin:w-1/2 xxlmax:w-full"
           >
             <h2 class="font-bold">{{ article.title }}</h2>
-            <p>by {{ article.author.name }}</p>
+            <p>by {{ authors.find((x) => x.ID === article.author).name }}</p>
             <p class="font-bold text-gray-600 text-sm">
               {{ article.description }}
             </p>
@@ -40,7 +48,14 @@
       >
         <NuxtLink :to="`/blog/tag/${tag.slug}`" class="">
           <p
-            class="font-bold text-gray-600 uppercase tracking-wider font-medium text-ss"
+            class="
+              font-bold
+              text-gray-600
+              uppercase
+              tracking-wider
+              font-medium
+              text-ss
+            "
           >
             {{ tag.name }}
           </p>
@@ -70,17 +85,29 @@
 <script>
 export default {
   async asyncData({ $content, params }) {
-    const articles = await $content('articles')
-      .only(['title', 'description', 'img', 'slug', 'author'])
-      .sortBy('createdAt', 'desc')
+    const blogs = await $content('contentrain')
+      .where({
+        slug: 'Blogs'
+      })
       .fetch()
     const tags = await $content('tags')
       .only(['name', 'description', 'img', 'slug'])
       .sortBy('createdAt', 'asc')
       .fetch()
+    const authors = await $content('contentrain')
+      .where({
+        slug: 'Authors'
+      })
+      .fetch()
     return {
-      articles,
-      tags
+      tags,
+      authors,
+      blogs
+    }
+  },
+  methods: {
+    importImage(img) {
+      return require('../' + img)
     }
   }
 }
